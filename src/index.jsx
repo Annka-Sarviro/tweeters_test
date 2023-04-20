9f7f1408c12b7421f5e1a05c8bfa3d809c6da4be
Ann Sarviro <annka_nuha@ai.ua>
2023-04-20 12:39:03 +0300

    fix 404

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tweeters_test">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
