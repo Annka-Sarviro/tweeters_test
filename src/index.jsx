37247c8b274bb119b269c9c90e4ad56249637b1e
Ann Sarviro <annka_nuha@ai.ua>
2023-04-20 12:21:47 +0300

    fix 404

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "normalize.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/tweeters_test">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
